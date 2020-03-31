673fb391737ed4562731b76de1f7ebee030427a1
poseidon9 <poseidonthenine@gmail.comm>
2020-03-30 23:19:54 -0500

    dawn of a new project - 30 marzo 2020

--- player/player.gd
+++ player/player.gd
@@ -7,12 +7,18 @@ const GRAVITY_VEC = Vector2(0, 900)
 const FLOOR_NORMAL = Vector2(0, -1)
 const SLOPE_SLIDE_STOP = 25.0
 const WALK_SPEED = 250 # pixels/sec
+const RUN_SPEED = 600 # pixels/sec
 const JUMP_SPEED = 480
 const SIDING_CHANGE_SPEED = 10
 const BULLET_VELOCITY = 1000
 const SHOOT_TIME_SHOW_WEAPON = 0.2
+const MAX_MULTI_JUMP = 2
+const COMBINATION_TIMING_TRESHOLD = 1
 
-var DOUBLE_JUMP = true
+var DASHING = false
+var MULTI_JUMP = 2
+var ACTIONS = []
+var last_action_time = 99999
 
 var linear_vel = Vector2()
 var shoot_time = 99999 # time since last shot
@@ -24,10 +30,21 @@ onready var sprite = $Sprite
 # cache bullet for fast access
 var Bullet = preload("res://player/Bullet.tscn")
 
+func add_action(action):
+	last_action_time = 0
+	if(not ACTIONS.has(action)):
+		ACTIONS.append(action)
+
 
 func _physics_process(delta):
+	print(ACTIONS)
 	# Increment counters
 	shoot_time += delta
+	last_action_time += delta
+
+	if(last_action_time > COMBINATION_TIMING_TRESHOLD):
+		last_action_time = 0
+		ACTIONS = []
 
 	### MOVEMENT ###
 
@@ -39,15 +56,17 @@ func _physics_process(delta):
 	var on_floor = is_on_floor()
 
 	if is_on_floor():
-			DOUBLE_JUMP = true
+		MULTI_JUMP = MAX_MULTI_JUMP
 
 	### CONTROL ###
 
 	# Horizontal movement
 	var target_speed = 0
 	if Input.is_action_pressed("move_left"):
+		add_action("left")
 		target_speed -= 1
 	if Input.is_action_pressed("move_right"):
+		add_action("right")
 		target_speed += 1
 
 	target_speed *= WALK_SPEED
@@ -56,18 +75,20 @@ func _physics_process(delta):
 	# Jumping
 	#if on_floor and Input.is_action_just_pressed("jump") :
 	if Input.is_action_just_pressed("jump") :
+		add_action("jump")
 		if on_floor:
 			linear_vel.y = -JUMP_SPEED
 			($SoundJump as AudioStreamPlayer2D).play()
 		else:
-			if(DOUBLE_JUMP):
+			if(MULTI_JUMP > 0):
 				linear_vel.y = -JUMP_SPEED
 				($SoundJump as AudioStreamPlayer2D).play()
-				DOUBLE_JUMP = false
+				MULTI_JUMP -= 1
 
 
 	# Shooting
 	if Input.is_action_just_pressed("shoot"):
+		add_action("shoot")
 		var bullet = Bullet.instance()
 		bullet.position = ($Sprite/BulletShoot as Position2D).global_position # use node for shoot position
 		bullet.linear_velocity = Vector2(sprite.scale.x * BULLET_VELOCITY, 0)
