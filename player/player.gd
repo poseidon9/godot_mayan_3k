608d44162d1ef810d289b1f458b4918479448fa4
poseidon9 <poseidonthenine@gmail.comm>
2020-04-13 20:14:52 -0500

    dash implemented - 13 abril 2020

--- player/player.gd
+++ player/player.gd
@@ -13,15 +13,19 @@ const SIDING_CHANGE_SPEED = 10
 const BULLET_VELOCITY = 1000
 const SHOOT_TIME_SHOW_WEAPON = 0.2
 const MAX_MULTI_JUMP = 2
-const COMBINATION_TIMING_TRESHOLD = 1
+const COMBINATION_TIMING_TRESHOLD = 0.3
+const MAX_DASHING_TIME = 0.25
 
 var DASHING = false
+var CAN_DASH = true
 var MULTI_JUMP = 2
 var ACTIONS = []
-var last_action_time = 99999
+var DASHING_TIME = 0
+var FACING = 1; #-1 left, 1 right
 
 var linear_vel = Vector2()
 var shoot_time = 99999 # time since last shot
+var last_action_time = 99999
 
 var anim = ""
 
@@ -37,7 +41,7 @@ func add_action(action):
 
 
 func _physics_process(delta):
-	print(ACTIONS)
+	#print(ACTIONS)
 	# Increment counters
 	shoot_time += delta
 	last_action_time += delta
@@ -57,17 +61,26 @@ func _physics_process(delta):
 
 	if is_on_floor():
 		MULTI_JUMP = MAX_MULTI_JUMP
+		DASHING = false
+		DASHING_TIME = 0
+		CAN_DASH = true
 
 	### CONTROL ###
 
 	# Horizontal movement
 	var target_speed = 0
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_just_pressed("move_left"):
 		add_action("left")
+	if Input.is_action_just_pressed("move_right"):
+		add_action("right")
+
+	if Input.is_action_pressed("move_left") and not DASHING:
 		target_speed -= 1
-	if Input.is_action_pressed("move_right"):
+		FACING = -1
+	if Input.is_action_pressed("move_right") and not DASHING:
 		add_action("right")
 		target_speed += 1
+		FACING = 1
 
 	target_speed *= WALK_SPEED
 	linear_vel.x = lerp(linear_vel.x, target_speed, 0.1)
@@ -81,6 +94,7 @@ func _physics_process(delta):
 			($SoundJump as AudioStreamPlayer2D).play()
 		else:
 			if(MULTI_JUMP > 0):
+				CAN_DASH = true
 				linear_vel.y = -JUMP_SPEED
 				($SoundJump as AudioStreamPlayer2D).play()
 				MULTI_JUMP -= 1
@@ -110,18 +124,31 @@ func _physics_process(delta):
 			sprite.scale.x = 1
 			new_anim = "run"
 	else:
-		# We want the character to immediately change facing side when the player
+		# We want the character to immediately change FACING side when the player
 		# tries to change direction, during air control.
 		# This allows for example the player to shoot quickly left then right.
-		if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right") and not DASHING:
 			sprite.scale.x = -1
-		if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		if Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left") and not DASHING:
 			sprite.scale.x = 1
+
+		if Input.is_key_pressed(KEY_X) and CAN_DASH:
+			DASHING = true
+			CAN_DASH = false
 
 		if linear_vel.y < 0:
 			new_anim = "jumping"
 		else:
 			new_anim = "falling"
+
+		if DASHING:
+			DASHING_TIME += delta
+			linear_vel.x = FACING * RUN_SPEED
+			linear_vel.y = -15 # why???
+
+			if DASHING_TIME > MAX_DASHING_TIME:
+				DASHING = false
+				DASHING_TIME = 0
 
 	if shoot_time < SHOOT_TIME_SHOW_WEAPON:
 		new_anim += "_weapon"
